4bffba1b26bad8f88fd76b2b7864bccafb0dd542
bobvantpadje <bobvantadje@gmail.com>
2017-05-18 14:18:14 +0200

    added inbox and outbox models

---
+++ app/Inbox.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inbox extends Model
+{
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Class',
+        'Coding',
+        'ID',
+        'Processed',
+        'ReceivingDateTime',
+        'RecipientID',
+        'SenderNumber',
+        'SMSCNumber',
+        'Text',
+        'TextDecoded',
+        'UDH',
+        'UpdatedInDB',
+    ];
+}
---
+++ app/Outbox.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Outbox extends Model
+{
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'Class',
+        'Coding',
+        'CreatorID',
+        'DeliveryReport',
+        'DestinationNumber',
+        'ID',
+        'InsertIntoDB',
+        'MultiPart',
+        'Priority',
+        'RelativeValidity',
+        'Retries',
+        'SendAfter',
+        'SendBefore',
+        'SenderID',
+        'SendingDateTime',
+        'SendingTimeOut',
+        'Text',
+        'TextDecoded',
+        'UDH',
+        'UpdatedInDB',
+    ];
+}
