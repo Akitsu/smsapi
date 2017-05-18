4db173562a43c9447cf10cb81325c1a5c52c3b4e
Anthony Sitaram <anthony.sitaram@hva.nl>
2017-05-18 17:01:30 +0200

    fix

--- app/Outbox.php
+++ app/Outbox.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Outbox extends Model
 {
     public $timestamps = false;
+    protected $table = 'outbox';
+
     /**
      * The attributes that are mass assignable.
      *
