2639e25c5b42e1b066d6ccaf6942acfa04181fc3
bobvantpadje <bobvantadje@gmail.com>
2017-05-19 12:12:00 +0200

    added routings

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+//MessageController
 $app->get('getallmessages', [
     'as' => 'allmessages', 'uses' => 'MessageController@getAllMessages'
 ]);
@@ -12,7 +13,6 @@ $app->post('getmessagesbyterm', [
 $app->post('getmessagesbyid', [
     'as' => 'getmessagesbyid', 'uses' => 'MessageController@getMessagesById'
 ]);
-
 $app->post('sendmessage', [
     'as' => 'sendmessage', 'uses' => 'MessageController@sendMessage'
 ]);
@@ -25,9 +25,25 @@ $app->post('markasunread', [
 $app->post('deletemessage', [
     'as' => 'deletemessage', 'uses' => 'MessageController@deleteMessage'
 ]);
+
+//PhoneController
 $app->get('getstatus', [
     'as' => 'getstatus', 'uses' => 'PhoneController@getStatus'
 ]);
+
+//SentMessageController
 $app->get('getsentmessages', [
     'as' => 'getsentmessages', 'uses' => 'SentMessageController@getSentMessages'
+]);
+$app->post('getsentmessagesbynumber', [
+    'as' => 'getsentmessagesbynumber', 'uses' => 'SentMessageController@getSentMessagesByNumber'
+]);
+$app->post('getsentmessagesbyterm', [
+    'as' => 'getsentmessagesbyterm', 'uses' => 'SentMessageController@getSentMessagesByTerm'
+]);
+$app->post('getsentmessagesbyid', [
+    'as' => 'getsentmessagesbyid', 'uses' => 'SentMessageController@getSentMessagesById'
+]);
+$app->post('deletesentmessage', [
+    'as' => 'deletesentmessage', 'uses' => 'SentMessageController@deleteSentMessage'
 ]);
