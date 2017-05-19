053ab9cae7444441e92f8f01f6c14e88c47c2977
Anthony Sitaram <anthony.sitaram@hva.nl>
2017-05-19 23:36:11 +0200

    c

--- app/User.php
+++ app/User.php
@@ -32,21 +32,11 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'password',
     ];
 
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT
-     *
-     * @return mixed
-     */
     public function getJWTIdentifier()
     {
-        return $this->getUserId();
+        return $this->getKey();
     }
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT
-     *
-     * @return array
-     */
     public function getJWTCustomClaims()
     {
         return [];
