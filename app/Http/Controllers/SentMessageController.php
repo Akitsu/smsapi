e848cbf74dd23aab7e20efb609849b54cede468e
bobvantpadje <bobvantadje@gmail.com>
2017-05-19 12:10:16 +0200

    added sentItem controller methods and did some small refactoring

--- app/Http/Controllers/SentMessageController.php
+++ app/Http/Controllers/SentMessageController.php
@@ -3,15 +3,33 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use App\Sentitems as Sentitems;
+use App\Sentitems as SentItems;
 
 class SentMessageController extends BaseController
 {
     public function getSentMessages()
     {
         return new JsonResponse(['messages' => Sentitems::get()]);
+    }
+
+    public function getSentMessagesByNumber(Request $request) {
+        return new JsonResponse(SentItems::where('DestinationNumber', '=', $request->input('number'))->get());
+    }
+
+    public function getSentMessagesById(Request $request) {
+        return new JsonResponse(SentItems::where('ID', '=', $request->input('id'))->get());
+    }
+
+    public function getSentMessagesByTerm(Request $request) {
+        return new JsonResponse(SentItems::where('TextDecoded', 'LIKE', '%' . $request->input('term') . "%")->get());
+    }
+
+    public function deleteSentMessage (Request $request) {
+        $sentMessage = SentItems::where('ID', $request->input('id'))
+            ->delete();
+
+        return new JsonResponse($sentMessage);
     }
 }
 
--- app/SentItems.php
+++ app/SentItems.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Sentitems extends Model
+class SentItems extends Model
 {
     public $timestamps = false;
     protected $table = 'sentitems';
