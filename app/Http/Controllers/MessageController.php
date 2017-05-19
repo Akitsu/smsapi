19e67c872cd0dccd6cfdf18e1cd345102e8b0186
maiko.kuipers <maiko.kuipers@hva.nl>
2017-05-19 15:30:26 +0200

    fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -13,7 +13,7 @@ class MessageController extends BaseController
 {
     public function getAllMessages()
     {
-        return new JsonResponse(['messages' => Inbox::get()]);
+        return new JsonResponse(['messages' => Inbox::toSql()]);
     }
 
     public function getMessagesByNumber(Request $request) {
